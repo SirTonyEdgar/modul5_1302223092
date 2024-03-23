4f51636d4d608e3b4fab198a269c4333d88a8cfb
SirTonyEdgar <daffamuhammad625@gmail.com>
2024-03-23 09:12:10 +0700

    Bagian 6

--- modul5_1302223092/Program.cs
+++ modul5_1302223092/Program.cs
@@ -10,5 +10,37 @@ class Penjumlahan
     static void Main(String[] args)
     {
         Console.WriteLine(JumlahTigaAngka<int>(13, 22, 23));
+
+        SimpleDataBase<string > data = new SimpleDataBase<string>();
+        data.addNewData("13");
+        data.addNewData("22");
+        data.addNewData("23");
+        data.printAllData();
+    }
+}
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void addNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData[i] + " yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
     }
 }
