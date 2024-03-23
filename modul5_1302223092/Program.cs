f9e32a8ff34e781c3cac9d6be2fe7b1c7f21ffd6
SirTonyEdgar <daffamuhammad625@gmail.com>
2024-03-23 08:17:03 +0700

    tes

--- modul5_1302223092/Program.cs
+++ modul5_1302223092/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Penjumlahan
+{
+    public void JumlahTigaAngka<T>(T x, T y, T z)
+    {
+        T store;
+        store = (x + y + z);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Penjumlahan penjumlahan = new Penjumlahan();
+        int x, y, z;
+        x = 13;
+        y = 2;
+        z = 22;
+
+        Console.WriteLine(penjumlahan.JumlahTigaAngka(x, y, z));
+    }
+}
